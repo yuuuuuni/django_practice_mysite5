b55448ccc4777ce115b0862c5a68b2688bf5ee49
yuuuuuni <yuuuuuni930302@gmail.com>
2022-01-14 13:52:31 +0900

    2-10 폼, 수동 폼 작성

--- pybo/forms.py
+++ pybo/forms.py
@@ -4,12 +4,16 @@ from pybo.models import Question
 
 class QuestionForm(forms.ModelForm):
     class Meta:
-        model = Question
-        fields = ['subject', 'content']
-        widgets = { # Meta 클래스의 widgets 속성을 지정하면 부트스트랩 사용 가능
-            'subject': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
-        }
+        model = Question # QuestionForm도 어떤 모델을 기준으로 알려줘야 폼을 만들어 줄 수 있으니!
+        fields = ['subject', 'content'] # create_date 속성은 데이터 저장 시점에 자동생성 되므로 등록하지 않음
+
+
+        # widgets = {  # Meta 클래스의 widgets 속성을 지정하면 부트스트랩 사용 가능
+        #    'subject': forms.TextInput(attrs={'class': 'form-control'}),
+        #    'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+        # }
+
+
         labels = {
             'subject': '제목',
             'content': '내용',
--- pybo/views.py
+++ pybo/views.py
@@ -13,7 +13,7 @@ def index(request):
     pybo 질문 목록
     """
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list} # 여기에서 부르는 키는 html에서 쓸 키, 값은 여기의 값
+    context = {'question_list': question_list}  # 여기에서 부르는 키는 html에서 쓸 키, 값은 여기의 값
     return render(request, 'pybo/question_list.html', context)
 
 
@@ -26,7 +26,7 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
-def answer_create(request, question_id): # request에는 사용자가 적은 답변 내용이 넘어옴
+def answer_create(request, question_id):  # request에는 사용자가 적은 답변 내용이 넘어옴
     """
     pybo 답변 등록
     """
@@ -40,14 +40,13 @@ def question_create(request):
     pybo 질문 등록
     """
     if request.method == 'POST':
-        form = QuestionForm(request.POST) # 데이터가 들어있는 QuestionForm을 form이라는 변수에 저장
-        if form.is_valid(): # QuestionForm이 형식에 맞게 제대로 되어 있다면
-            question = form.save(commit=False) # form을 임시저장해서 question이라는 변수에 넣어라
-            question.create_date = timezone.now() # 그 변수의 작성일시를 현재로 해라
-            question.save() # 저장해라
+        form = QuestionForm(request.POST)  # 데이터가 들어있는 post 방식의 QuestionForm을 form이라는 변수에 저장
+        if form.is_valid():  # QuestionForm이 형식에 맞게 제대로 되어 있다면
+            question = form.save(commit=False)  # form을 임시저장해서 question이라는 변수에 넣어라
+            question.create_date = timezone.now()  # 그 변수의 작성일시를 현재로 해라
+            question.save()  # 저장해라
             return redirect('pybo:index')
-    else: # 방식이 post가 아니면? get이겠지?
-        form = QuestionForm() # 데이터가 없는 빈 폼
+    else:  # 방식이 post가 아니면? get이겠지?
+        form = QuestionForm()  # 데이터가 없는 빈 폼
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
-
